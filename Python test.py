ac79105ce3dc3392a2a8b5fc756a0412d11b6b70
Elisama <88242218+ellievi@users.noreply.github.com>
2021-08-31 00:45:33 -0300

    Update Python test.py

--- Python test.py
+++ Python test.py
@@ -124,3 +124,136 @@ print('O valor do desconto é: {}. O valor final do produto é: {}.'.format(desc
 c = float(input('Digite uma temperatura em Celsius: '))
 f = (9 * c / 5) + 32
 print('Celsius: {}. Fahrenheite: {}.'.format(c, f))
+
+x = int(input('Digite um valor inteiro: '))
+y = int(input('Digite outro valor inteiro: '))
+if (x > y):
+    print('O primeiro valor é maior que o segundo!')
+
+x = int(input('Digite um valor inteiro: '))
+if (x % 2 == 0):
+    print('O número é par!')
+else:
+    print('O número é ímpar!')
+if (x % 7 == 0):
+    print('Múltiplo de 7')
+else:
+    print('Não é múltiplo de 7')
+
+x = True
+y = False
+print(not x)
+print(not y)
+
+x = True
+y = False
+print(x and y)
+
+x = True
+y = False
+print(x or y)
+
+x = 10
+y = 1
+res = not x > y
+print(res)
+
+x = 10
+y = 1
+z = 5.5
+res = x > y and z == y
+print(res)
+
+m1 = float(input('Digite a nota da 1° matéria: '))
+m2 = float(input('Digite a nota da 2° matéria: '))
+m3 = float(input('Digite a nota da 3° matéria: '))
+if m1 >= 7 and m2 >= 7 and m3 >= 7:
+    print('O aluno passou de ano.')
+else:
+    print('O aluno não passou de ano.')
+
+nasc = int(input('Qual seu ano de nascimento?'))
+ano = int(input('Em que ano estamos?'))
+idade = ano - nasc
+print('Você tem {} anos de idade.' .format(idade))
+if (idade >= 18):
+    print('Você já é de maior. Já pode tirar a carteira de motorista!')
+else:
+    print('Você ainda não atingiu a idade mínima -18- para tirar a cateira de motorista.')
+
+print('Escolha o que deseja comprar: ')
+print('1 - Maça')
+print('2 - Laranja')
+print('3 - Banana')
+produto = int(input('Qual sua escolha? '))
+qtd = int(input('Quantas unidades? '))
+if (produto == 1):
+    pagar = qtd * 2.3
+    print('Você comprou {} maças. Total á pagar: {}'.format(qtd, pagar))
+else:
+    if (produto == 2):
+        pagar = qtd * 3.6
+        print('Você comprou {} laranjas. Total à pagar: {}'.format(qtd, pagar))
+    else:
+        if (produto == 3):
+            pagar = qtd * 1.85
+            print('Você comprou {} bananas. Total à pagar: {}'.format(qtd, pagar))
+        else:
+            print('Produto não existente')
+
+            a = int(input('Digite o 1° lado do triângulo: '))
+b = int(input('Digite o 2° lado do triângulo: '))
+c = int(input('Digite o 3° lado do triângulo: '))
+if a > 0 and b > 0 and c > 0:
+    if a + b > c and a + c > b and b + c > a:
+        if a != b and a != c and b != c:
+            print('Triângulo escaleno!')
+        else:
+            if a == b and b == c:
+                print('Triângulo equilátero')
+            else:
+                print('Triângulo isósceles')
+    else:
+        print('Ao menos um dos valores indicados não servem para forma um triângulo.')
+else:
+    print('Ao menos um dos valores indicados não servem para forma um triângulo.')
+
+print('Escolha o que deseja comprar: ')
+print('1 - Maça')
+print('2 - Laranja')
+print('3 - Banana')
+produto = int(input('Qual sua escolha? '))
+qtd = int(input('Quantas unidades? '))
+if (produto == 1):
+    pagar = qtd * 2.3
+    print('Você comprou {} maças. Total á pagar: {}'.format(qtd, pagar))
+elif (produto == 2):
+    pagar = qtd * 3.6
+    print('Você comprou {} laranjas. Total à pagar: {}'.format(qtd, pagar))
+elif (produto == 3):
+    pagar = qtd * 1.85
+    print('Você comprou {} bananas. Total à pagar: {}'.format(qtd, pagar))
+else:
+    print('Produto não existente')
+
+    print('CALCULADORA:')
+print('+ Adição')
+print('- Subtração')
+print('* Multiplicação')
+print('/ Divisão')
+print('Pressione outra tecla para sair ')
+op = input('Qual operação deseja fazer? ')
+x = int(input('Digite o primeiro valor: '))
+y = int(input('Digite o segundo valor: '))
+if (op == '+'):
+    res = x + y
+    print('Resultado: {} + {} = {}'.format(x, y, res))
+elif (op == '-'):
+    res = x - y
+    print('Resultado: {} - {} = {}'.format(x, y, res))
+elif (op == '*'):
+    res = x * y
+    print('Resultado: {} * {} = {}'.format(x, y, res))
+elif (op == '/'):
+    res = x / y
+    print('Resultado: {} / {} = {}'.format(x, y, res))
