42e561de8e030e65ef64a664850de534c166e51a
Elisama <88242218+ellievi@users.noreply.github.com>
2021-08-31 14:47:19 -0300

    Update Python test.py

--- Python test.py
+++ Python test.py
@@ -288,4 +288,27 @@ elif (op == 4):
   print('Produto parcelado em 10x. Total a pagar: {}. Valor da parcela: {}'.format(valor_final, parcela))
 else:
   print('Operação inválida!')    
+  
+  kwh = float(input('Quantos kwh? '))
+tipo = input('Qual o tipo de instalação? (R, C ou I) ')
+
+if (tipo == 'R'):
+    if kwh <= 500:
+        preco = 0.4
+    else:
+        preco = 0.65
+
+elif (tipo == 'C'):
+    if kwh <= 1000:
+        preco = 0.55
+    else:
+        preco = 0.6
+
+elif (tipo == 'I'):
+    if kwh <= 5000:
+        preco = 0.55
+    else:
+        preco = 0.6
+else:
+    print('Instalação inválida.')
     
